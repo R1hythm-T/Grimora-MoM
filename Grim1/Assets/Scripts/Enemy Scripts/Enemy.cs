5e55d6b9dca648d96c0056bdc4c90906c16b7d61
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-11-25 00:05:43 +0530

    battleprep 2

--- Grim1/Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Grim1/Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : Entity
     [Header("Move Info")]
     public float moveSpeed;
     public float idleTime;
+    [Header("Attack Info")]
+    public float attackDistance;
     public EnemyStateMachine stateMachine { get; private set; }
 
     protected override void Awake()
@@ -23,4 +25,11 @@ public class Enemy : Entity
     }
 
     public virtual RaycastHit2D IsPlayerDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, 50, whatIsPlayer);
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + attackDistance * facingDir, transform.position.y, 0));
+    }
 }
--- Grim1/Assets/Scripts/Enemy Scripts/SkeletonGroundedState.cs
+++ Grim1/Assets/Scripts/Enemy Scripts/SkeletonGroundedState.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkeletonGroundedState : MonoBehaviour
+public class SkeletonGroundedState : EnemyState
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Enemy_Skeleton enemy;
+    public SkeletonGroundedState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Skeleton enemy) : base(_enemyBase, _stateMachine, _animBoolName)
     {
-        
+        this.enemy = enemy;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Enter()
     {
-        
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (enemy.IsPlayerDetected())
+            stateMachine.ChangeState(enemy.battleState);
     }
 }

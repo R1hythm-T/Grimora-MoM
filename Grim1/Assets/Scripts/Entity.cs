7f76ad2f8411b61246f9003acfacb65d82cb2bcc
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-11-25 15:54:31 +0530

    hit flash fx

--- Grim1/Assets/Scripts/Entity.cs
+++ Grim1/Assets/Scripts/Entity.cs
@@ -7,6 +7,7 @@ public class Entity : MonoBehaviour
     #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
+    public EntityFX fx { get; private set; }
 
     #endregion
 
@@ -29,6 +30,7 @@ public class Entity : MonoBehaviour
 
     protected virtual void Start()
     {
+        fx = GetComponent<EntityFX>();
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -40,6 +42,7 @@ public class Entity : MonoBehaviour
 
     public virtual void Damage()
     {
+        fx.StartCoroutine("FlashFX");
         Debug.Log(gameObject.name + " was damaged");
     }
 

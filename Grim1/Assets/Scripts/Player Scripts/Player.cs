7cae2b69a0871ef3fed45dee06c7277f62ce9a63
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-11-29 00:15:14 +0530

    Entire counter attack mechanic complete

--- Grim1/Assets/Scripts/Player Scripts/Player.cs
+++ Grim1/Assets/Scripts/Player Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : Entity
 {
     [Header("Attack Details")]
     public Vector2[] attackMovement;
+    public float counterAttackDuration = .2f;
     
     public bool isBusy {  get; private set; }
 
@@ -33,6 +34,7 @@ public class Player : Entity
 
 
     public PlayerPrimaryAttackState primaryAttack { get; private set; }
+    public PlayerCounterAttackState counterAttack { get; private set; }
 
     #endregion
 
@@ -50,6 +52,7 @@ public class Player : Entity
         wallJump = new PlayerWallJumpState(this, stateMachine, "Jump");
 
         primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
+        counterAttack = new PlayerCounterAttackState(this, stateMachine, "CounterAttack");
     }
 
     protected override void Start()

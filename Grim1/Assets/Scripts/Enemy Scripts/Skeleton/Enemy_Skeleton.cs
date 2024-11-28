ebb340926f2ec21703e90e3725f0f5f52095796b
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-11-28 21:53:21 +0530

    added counterattack window

--- Grim1/Assets/Scripts/Enemy Scripts/Skeleton/Enemy_Skeleton.cs
+++ Grim1/Assets/Scripts/Enemy Scripts/Skeleton/Enemy_Skeleton.cs
@@ -37,4 +37,15 @@ public class Enemy_Skeleton : Enemy
             stateMachine.ChangeState(stunnedState);
         }
     }
+
+    protected override bool CanBeStunned()
+    {
+        if (base.CanBeStunned())
+        {
+            stateMachine.ChangeState(stunnedState);
+            return true;
+        }
+
+        return false;
+    }
 }

fd0bbb92fc8ca8ac899ea659d1b9b557f52749d1
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-11-29 16:46:49 +0530

    Improved clone to attack closest enemy

--- Grim1/Assets/Scripts/Skills/CloneSkill.cs
+++ Grim1/Assets/Scripts/Skills/CloneSkill.cs
@@ -7,10 +7,12 @@ public class CloneSkill : Skill
     [Header("Clone Info")]
     [SerializeField] private GameObject clonePrefab;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack;
 
     public void CreateClone(Transform _clonePosition)
     {
         GameObject newClone = Instantiate(clonePrefab);
-        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration);
+        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack);
     }
 }

8697e3e56f38804674c92dd89aafab38e567f4b2
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-12-06 00:56:34 +0530

    improved animations and limited thrown swords to 1
    also it comes back on 2nd right click

--- Grim1/Assets/Scripts/Player Scripts/Player.cs
+++ Grim1/Assets/Scripts/Player Scripts/Player.cs
@@ -20,6 +20,7 @@ public class Player : Entity
     public float dashDir { get; private set; }
 
     public SkillManager skill {  get; private set; }
+    public GameObject sword { get; private set; }
 
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
@@ -73,6 +74,16 @@ public class Player : Entity
         base.Update();
         stateMachine.currentState.Update();
         CheckForDashInput();
+    }
+
+    public void AssignNewSword(GameObject _newSword)
+    {
+        sword = _newSword;
+    }
+
+    public void ClearSword()
+    {
+        Destroy(sword);
     }
 
     public IEnumerator BusyFor(float _seconds)
--- Grim1/Assets/Scripts/Player Scripts/PlayerGroundedState.cs
+++ Grim1/Assets/Scripts/Player Scripts/PlayerGroundedState.cs
@@ -23,7 +23,7 @@ public class PlayerGroundedState : PlayerState
     {
         base.Update();
 
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.GetKeyDown(KeyCode.Mouse1) && HasNoSword())
             stateMachine.ChangeState(player.aimSword);
 
         if (Input.GetKeyDown(KeyCode.Q))
@@ -37,5 +37,16 @@ public class PlayerGroundedState : PlayerState
 
         if (Input.GetKeyDown(KeyCode.Space) && player.IsGroundDetected())
             stateMachine.ChangeState(player.jumpState);
+    }
+
+    private bool HasNoSword()
+    {
+        if (!player.sword)
+        {
+            return true;
+        }
+
+        player.sword.GetComponent<SwordSkillController>().ReturnSword();
+        return false;
     }
 }

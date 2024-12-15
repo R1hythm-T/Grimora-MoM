0dc6c8d61f606931704666ac49e932f8ee2e77a3
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-12-15 22:36:44 +0530

    added blackhole

---
+++ Grim1/Assets/Scripts/Skills/Skill Controllers/BlackholeSkillController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeSkillController : MonoBehaviour
+{
+    [SerializeField] private GameObject hotKeyPrefab;
+    [SerializeField] private List<KeyCode> keyCodeList;
+
+    public float maxSize;
+    public float growSpeed;
+    public bool canGrow;
+
+    private List<Transform> targets = new List<Transform>();
+
+    private void Update()
+    {
+        if (canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Enemy>() != null)
+        {
+            collision.GetComponent<Enemy>().FreezeTime(true);
+            CreateHotKey(collision);
+        }
+    }
+
+    private void CreateHotKey(Collider2D collision)
+    {
+        if (keyCodeList.Count <= 0)
+        {
+            Debug.LogWarning("Not enough hotkeys in the key code list!");
+            return;
+        }
+
+        GameObject newHotKey = Instantiate(hotKeyPrefab, collision.transform.position + new Vector3(0, 2), Quaternion.identity);
+
+        KeyCode chosenKey = keyCodeList[Random.Range(0, keyCodeList.Count)];
+        keyCodeList.Remove(chosenKey);
+
+        BlackholeHotkeyController newHotKeyScript = newHotKey.GetComponent<BlackholeHotkeyController>();
+        newHotKeyScript.SetupHotKey(chosenKey, collision.transform, this);
+    }
+
+    public void AddEnemyToList(Transform _enemyTransform) => targets.Add(_enemyTransform);
+}

c2e1643a37cd121779960adf69b7b4286de31b33
R1hythm-T <ruchirtiwari2003@gmail.com>
2024-12-27 22:00:43 +0530

    added elemental ailments

--- Grim1/Assets/Scripts/CharacterStats.cs
+++ Grim1/Assets/Scripts/CharacterStats.cs
@@ -1,25 +1,82 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
+    [Header("Major Stats")]
+    public Stat strength; //increase both damage and crit damage
+    public Stat agility; //increase both evasion and crit chance
+    public Stat intelligence; //increase both magic damage and magic resistance
+    public Stat vitality; //increase health
+
+    [Header("Offensive Stats")]
     public Stat damage;
+    public Stat critChance;
+    public Stat critPower; //default 200%
+
+    [Header("Defensive Stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+    public Stat magicResistance;
+
+    [Header("Magic Stats")]
+    public Stat fireDamage;
+    public Stat iceDamage;
+    public Stat electricDamage;
+
+    public bool isIgnited;
+    public bool isChilled;
+    public bool isShocked;
 
     [SerializeField] private int currentHealth;
     // Start is called before the first frame update
     protected virtual void Start()
     {
+        critPower.SetDefaultValue(200);
         currentHealth = maxHealth.GetValue();
     }
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
-        _targetStats.TakeDamage(totalDamage);
+
+        if (CanCrit())
+            totalDamage = CalculateCriticalDamage(totalDamage);
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
+        //_targetStats.TakeDamage(totalDamage);
+        DoMagicalDamage(_targetStats);
+    }
+    public virtual void DoMagicalDamage(CharacterStats _targetStats)
+    {
+        int _fireDamage = fireDamage.GetValue();
+        int _iceDamage = iceDamage.GetValue();
+        int _electricDamage = electricDamage.GetValue();
+
+        int totalMagicalDamage = _fireDamage + _iceDamage + _electricDamage + intelligence.GetValue();
+
+        totalMagicalDamage = CheckTargetResistance(_targetStats, totalMagicalDamage);
+        _targetStats.TakeDamage(totalMagicalDamage);
+    }
+
+    private static int CheckTargetResistance(CharacterStats _targetStats, int totalMagicalDamage)
+    {
+        totalMagicalDamage -= _targetStats.magicResistance.GetValue() + (_targetStats.intelligence.GetValue() * 3);
+        totalMagicalDamage = Mathf.Clamp(totalMagicalDamage, 0, int.MaxValue);
+        return totalMagicalDamage;
+    }
+
+    public void ApplyAilments(bool _ignite, bool _chill, bool _shock)
+    {
+        if(isIgnited || isChilled || isShocked)
+            return;
+
+        isIgnited = _ignite;
+        isChilled = _chill;
+        isShocked = _shock;
     }
 
     public virtual void TakeDamage(int _damage)
@@ -33,5 +90,43 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
         //throw new NotImplementedException();
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalevasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+        if (Random.Range(0, 100) < totalevasion)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CanCrit()
+    {
+        int totalCriticalChance = critChance.GetValue() + agility.GetValue();
+
+        if (Random.Range(0, 100) <= totalCriticalChance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int CalculateCriticalDamage(int _damage)
+    {
+        float totalCritPower = (critPower.GetValue() + strength.GetValue()) * .01f;
+        float critDamage = totalCritPower * _damage;
+
+        return Mathf.RoundToInt(critDamage);
     }
 }

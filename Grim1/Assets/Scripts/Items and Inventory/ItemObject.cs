b575db4f88d3a2812ef1892f558f0dce29e7bdf8
Jake101 <138904409+Jake1-0-1@users.noreply.github.com>
2025-05-03 02:20:26 +0530

    item drop from enemies
    114 done

---
+++ Grim1/Assets/Scripts/Items and Inventory/ItemObject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
+
+public class ItemObject : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private ItemData itemData;
+
+
+    private void SetupVisuals()
+    {
+        if (itemData == null)
+            return;
+
+        GetComponent<SpriteRenderer>().sprite = itemData.icon;
+        gameObject.name = "Item object - " + itemData.itemName;
+    }
+
+    
+
+    public void SetupItem(ItemData _itemData, Vector2 _velocity)
+    {
+        itemData = _itemData;
+        rb.velocity = _velocity;
+
+        SetupVisuals();
+    }
+    public void PickupItem()
+    {
+        Inventory.Instance.AddItem(itemData);
+        Destroy(gameObject);
+    }
+}

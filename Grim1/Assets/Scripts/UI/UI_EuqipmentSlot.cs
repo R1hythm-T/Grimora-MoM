c7fda42bfc184ba477964dff4f08a2a95e982c17
Jake101 <138904409+Jake1-0-1@users.noreply.github.com>
2025-05-02 16:36:15 +0530

    added unequip on inventory items
    112 done

--- Grim1/Assets/Scripts/UI/UI_EuqipmentSlot.cs
+++ Grim1/Assets/Scripts/UI/UI_EuqipmentSlot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UI_EuqipmentSlot : UI_ItemSlot
 {
@@ -9,5 +10,12 @@ public class UI_EuqipmentSlot : UI_ItemSlot
     private void OnValidate()
     {
         gameObject.name= "Euqipment slot - " + slotType.ToString();
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        Inventory.Instance.UnequipItem(item.data as ItemData_Equipment);
+        Inventory.Instance.AddItem(item.data as ItemData_Equipment);
+        CleanUpSlot();
     }
 }
